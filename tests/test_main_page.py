fdb96786563c6dd48225de1776c8476600b74b5f
slazarska <slazarska@protonmail.com>
2023-02-27 18:31:53 +0300

    updated main page test

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -1,6 +1,14 @@
 from homeless_test_python.model import app
+import allure
+from allure_commons.types import Severity
 
 
+@allure.tag("UI")
+@allure.severity(Severity.BLOCKER)
+@allure.label("owner", "slazarska")
+@allure.feature("Main Functionality")
+@allure.story("Open Main Page")
+@allure.description("Open main page and check the main page is loaded")
 def test_open_main_page():
     app.mainpage.open_main_page()
     app.mainpage.check_main_page_is_opened()
