d2281a0cebc7ef6c1dbc4b9f15d81794a1b15737
slazarska <slazarska@protonmail.com>
2023-03-21 19:45:25 +0300

    refactoring test_main_page.py

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -4,12 +4,11 @@ from allure_commons.types import Severity
 from homeless_test_python.model.pages import mainpage
 
 
-@allure.tag("UI")
 @allure.severity(Severity.BLOCKER)
 @allure.label("owner", "slazarska")
-@allure.feature("Main Functionality")
-@allure.story("Open Main Page")
-@allure.description("Open main page and check the main page is loaded")
+@allure.feature("Основная функциональность")
+@allure.story("Главная страница")
+@allure.description("Тест открывает главную страницу сайта и проверяет, что страница загружена")
 def test_open_main_page():
     mainpage.open_main_page()
     mainpage.check_main_page_is_opened()
