633247346b0c4aef1622f43fcfc92a3c22d37b68
slazarska <slazarska@protonmail.com>
2023-03-31 15:31:55 +0300

    fixed conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,46 +5,13 @@ from selenium.webdriver.chrome.options import Options
 
 from homeless_test_python.utils import attach
 
-DEFAULT_BROWSER = 'chrome'
-DEFAULT_BROWSER_VERSION = '98.0'
-
-
-def pytest_addoption(parser):
-    parser.addoption(
-        '--browser_name',
-        default=DEFAULT_BROWSER,
-        help='web: browser (chrome | firefox | orepa| safari)'
-    )
-
-    parser.addoption(
-        '--browser_version',
-        default=DEFAULT_BROWSER_VERSION,
-        help='web: browser version (chrome: 100.0; if  firefox: 98.0)'
-    )
-
-
-@pytest.fixture(scope='session')
-def get_option_browser_name(request):
-    return request.config.getoption('--browser_name')
-
-
-@pytest.fixture(scope='session')
-def get_option_browser_version(request):
-    return request.config.getoption('--browser_version')
-
 
 @pytest.fixture(autouse=True)
-def browser_management(get_option_browser_name, get_option_browser_version):
-    browser_name = get_option_browser_name
-    browser_name = browser_name if browser_name != '' else DEFAULT_BROWSER
-
-    browser_version = get_option_browser_version
-    browser_version = browser_version if browser_version != "" else DEFAULT_BROWSER_VERSION
-
+def browser_management():
     options = Options()
     selenoid_capabilities = {
-        "browserName": browser_name,
-        "browserVersion": browser_version,
+        "browserName": "chrome",
+        "browserVersion": "100.0",
         "selenoid:options": {
             "enableVNC": True,
             "enableVideo": True
